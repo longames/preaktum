3e18e4d45b9faa7841891858397cabd26e39a676
Hime-Hime <33797415+Hime-Hime@users.noreply.github.com>
2017-11-23 21:17:50 +0700

    server with db
    recieve id->forms json from database and sends it

--- Server/server.py
+++ Server/server.py
@@ -2,25 +2,16 @@
 
 import socket
 import json
-import numpy
-import argparse
-import cv2
+from sqldb import select_from_id
 sock = socket.socket()
 port = 9090
-sock.bind(('', port))
+sock.bind(('127.0.0.1', port))
 sock.listen(1)
 conn, addr = sock.accept()
 
 print ('connected:', addr)
-
 while True:
 	data = conn.recv(1024)
 	if data:
-		d=json.loads(data)
-		conn.send(bytes(d["type"],'utf-8'))
-		if d["type"]=="basket":
-			conn.send(bytes(d["products"]["name"],'utf-8'))
-			conn.send(bytes(str(d["totalPrice"]),'utf-8'))
-		elif d["type"]=="image":
-			conn.send(bytes(str(d["imageBase64"]),'utf-8'))
+		conn.send(bytes(select_from_id(data),'utf-8'))
 conn.close()
