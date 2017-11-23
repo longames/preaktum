e7ecd071f34fc9026f2d2d7dd732df1def086260
Hime-Hime <33797415+Hime-Hime@users.noreply.github.com>
2017-11-23 23:11:47 +0700

    sql and json
    fixed some potential encoding bugs

--- Server/sqldb.py
+++ Server/sqldb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#-*- condig: utf-8 -*-
 import pymysql
 import json
 db = pymysql.connect(host='localhost',
@@ -6,12 +7,12 @@ db = pymysql.connect(host='localhost',
                              password='top4ek',
                              db='shop',
                              charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+                             cursorclass=pymysql.cursors.DictCursor,
+                             use_unicode=True)
 cur=db.cursor()
 def select_from_id (id_rec):
 	cur.execute("""SELECT * FROM products WHERE id = %s""",(id_rec))
 	data = cur.fetchone()
-	print (data)
-	jdata = json.dumps(data)
+	jdata = json.dumps(data, ensure_ascii=False)
 	return(jdata)
 select_from_id(1)
